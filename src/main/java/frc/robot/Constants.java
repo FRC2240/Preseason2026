1ab895225f67ece21ee665d5785abdc9234e5258
Tiernan Pian <tiernan.pian@gmail.com>
2025-10-25 10:51:24 -0600

    dependancies

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,14 +6,18 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
 import frc.robot.generated.TunerConstants;
 
 public class Constants {
-    public static class Robot {
-        // kSpeedAt12Volts desired top speed;
+ public static class Robot {
+     // kSpeedAt12Volts desired top speed;
         public static double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
         public static double MaxAngularRate = RotationsPerSecond.of(1).in(RadiansPerSecond);
 
@@ -26,5 +30,26 @@ public class Constants {
         public static Angle RotationDeadband = Degrees.of(0.05);
         public static double ControllerCooldown = 0.3;
         public static double ControllerThreshold = 0.2;
+    }
+
+    public static class Vision {
+        //stores tag layout for the current year's feild
+        public static AprilTagFieldLayout APRIL_TAG_LAYOUT = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+    
+        //assuming stores distance of cameras from center of bot
+        public static Transform3d CAMERA_0_POS = new Transform3d(-0.272575, 0.2413, 0.520699, new Rotation3d(Degrees.of(0), Degrees.of(-32), Degrees.of(-20))); 
+        public static Transform3d CAMERA_1_POS = new Transform3d(0.272575, 0.2413, 0.510699, new Rotation3d(Degrees.of(0), Degrees.of(-32), Degrees.of(20))); 
+    
+        public static double MAX_UNCERTAINTY = 0.3; // TBD
+        public static double MAX_Z_ERROR = 0.75; //TBD
+    
+        // Standard deviation coefficents, for 1 meter distance and 1 tag
+        // (Adjusted automatically based on distance and # of tags)
+        public static double LINEAR_STDEV_COEFF = 0.02; // Meters
+        public static double ANGULAR_STDEV_COEFF = 0.06; // Radians
+    
+        // Multipliers to apply for MegaTag 2 observations
+        public static double LINEAR_STDEV_MEGATAG_2_COEFF = 0.5; // More stable than full 3D solve
+        public static double ANGULAR_STDEV_MEGATAG_2_COEFF = Double.POSITIVE_INFINITY; // No rotation data available    
     }
 }
