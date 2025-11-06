15dc300d2f98ec7dde968e618308fad15e944515
DylanACollins <962253@scienceandtech.org>
2025-11-05 17:29:55 -0700

    hi

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.AngleUnit;
+import edu.wpi.first.units.measure.Angle;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public class Climber extends SubsystemBase{
+    public TalonFX motorC = new TalonFX(67); //Change id (Climber) 
+    
+    MotionMagicTorqueCurrentFOC req = new MotionMagicTorqueCurrentFOC(0);
+
+    public void setClimberPosition(AngleUnit rotations) {
+        motorC.setControl(req.withPosition(Angle.ofBaseUnits(10, rotations)));
+    }
+
+    public StatusSignal<Angle> getPosition(){
+        return motorC.getPosition();
+    } 
+    
+    public Command climberAngle(AngleUnit rotations) {
+        return Commands.runOnce(() -> {
+            setClimberPosition(rotations);
+        }, this); 
+
+    }
+  }
