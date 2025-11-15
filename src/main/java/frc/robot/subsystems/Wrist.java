c4ef851ede76fe6fd2f1cd9ab58a9572bb2dd946
exsfin <nrg8011@gmail.com>
2025-11-15 14:59:45 -0700

    wrist smart dash and negate gravity

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -5,29 +5,34 @@ import frc.robot.Constants;
 
 import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class Wrist extends SubsystemBase {
-    private TalonFX wrist = new TalonFX(Constants.Wrist.WRIST_MOTOR_ID);
+    private TalonFX motor = new TalonFX(Constants.Wrist.WRIST_MOTOR_ID);
     MotionMagicTorqueCurrentFOC req = new MotionMagicTorqueCurrentFOC(0);
 
     public Wrist() {
         TalonFXConfiguration cfg = new TalonFXConfiguration();
 
         cfg.CurrentLimits.SupplyCurrentLimit = 3;
+        cfg.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
 
-        wrist.getConfigurator().apply(cfg);
+        motor.getConfigurator().apply(cfg);
+
     }
 
     public void setPosition(Angle rotations) {
-        wrist.setControl(req.withPosition(rotations));
+        motor.setControl(req.withPosition(rotations));
     }
 
     public Angle getPosition() {
-        return wrist.getPosition().getValue();
+        return motor.getPosition().getValue();
     }
 
     public Command setPositionCommand(Angle rotations) {
@@ -37,5 +42,9 @@ public class Wrist extends SubsystemBase {
             return getPosition().isNear(rotations, Constants.Wrist.POSITION_THRESHOLD);
         });
     }
-
+    
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Wrist/position", motor.getPosition().getValueAsDouble());
+    }
 }
